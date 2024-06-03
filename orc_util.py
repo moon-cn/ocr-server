6a44338468b7238fd176465662078033e09dbde7
jiangtao <410518072@qq.com>
2024-06-03 19:36:43 +0800

    init

--- orc_util.py
+++ orc_util.py
@@ -1,10 +1,10 @@
 from paddleocr import PaddleOCR
 
 
-ocr = PaddleOCR(use_angle_cls=True, lang="ch")  # need to run only once to download and load model into memory
 
 
 def parse(image_path):
+    ocr = PaddleOCR(use_angle_cls=True, lang="ch")  # need to run only once to download and load model into memory
     result = ocr.ocr(image_path, cls=True)
     return result
 
